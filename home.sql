be67956a4e82a25d0499717aa4a26aefada719a9
Tiallysson Costa <tiallysson@proton.me>
2024-06-18 18:24:02 -0300

    Fiz algumas alterações de sintaxes incorretas

--- home.sql
+++ home.sql
@@ -8,18 +8,17 @@ create table tb_credenciais (
     email varchar not null,
     senha varchar not null,
 
-    foreign key (id_usuario) references tb_usuário(id_usuario)
 )
 
 create table tb_usuarios (
     id_usuario serial primary key,
-    credencial serial,
+    credencial serial unique,
     nome varchar(50) not null,
     sobrenome varchar(50) not null,
     cpf varchar(11) unique not null,
     data_cadastro timestamp defaut NOW,
     contato serial,
-    endereço serial,
+    endereco serial,
 
     -- Aqui ficam as referências desta tabela com outras (FK)
     foreign key (credencial) references tb_credenciais (id_credencial),
