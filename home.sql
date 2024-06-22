f3851eb3bfa8d3aa96baa0cac10465b7f92511a9
Tiallysson Costa <tiallysson@proton.me>
2024-06-22 19:01:15 -0300

    Adicionei endereço de cobrança e alterei no código, o local de algumas tabelas

--- home.sql
+++ home.sql
@@ -10,21 +10,21 @@ create table tb_enderecos (
     cidade varchar(50) not null,
     estado varchar(50) not null,
     numero varchar(10),
-    complemento varchar(100)
+    complemento varchar
 );
 
-create table tb_credenciais (
-    id_credencial serial primary key,
-    email varchar not null,
-    senha varchar not null
-);
+-- Adicionei o endereço de cobrança para caso o cliente necessite.
+-- Pode não se encaixar no projeto, e vai ser discutido posteriormente. (tiallysson@proton.me)
 
-create table tb_contato (
-    id_contato serial primary key,
-    celular varchar (14) unique not null,
-    email int not null,
-
-    foreign key (email) references tb_credenciais(email)
+create table tb_endereco_cobranca (
+    id_endereco_cb serial primary key,
+    cep varchar(9) not null,
+    rua varchar(100) not null,
+    bairro varchar(100), not null,
+    cidade varchar(50) not null,
+    estado varchar(50) not null,
+    numero varchar(10),
+    complemento varchar
 );
 
 create table tb_pedidos (
@@ -47,18 +47,37 @@ create table tb_produtos (
 
 create table tb_usuarios (
     id_usuario serial primary key,
-    id_credencial serial unique,
     nome varchar(50) not null,
     sobrenome varchar(50) not null,
     cpf varchar(11) unique not null,
     data_cadastro timestamp defaut NOW,
     contato int,
     endereco int,
+    endereco_cobranca int,
 
     -- Aqui ficam as referências desta tabela com outras (FK)
+    foreign key (endereco_cobranca) references tb_endereco_cobranca (id_endereco_cb),
     foreign key (contato) references tb_contato (id_contato),
     foreign key (endereco) references tb_enderecos (id_endereco),
-    foreign key (id_usuario) references tb_credenciais (id_usuario),
-    foreign key (id_redencial) references tb_credenciais (id_credencial),
     foreign key (id_usuario) references tb_pedidos (id_Pedidos)
+);
+
+-- Modifiquei o local dessas tabelas pois iriam apresentar problemas posteriormente
+-- Ainda irá ser analisado posteriormente (tiallysson@proton.me)
+
+create table tb_credenciais (
+    id_credencial serial primary key,
+    id_usuario serial unique,
+    email varchar not null,
+    senha varchar not null,
+
+    foreign key (id_usuario) references tb_usuarios (id_usuario)
+);
+
+create table tb_contato (
+    id_contato serial primary key,
+    celular varchar(14) unique not null,
+    email int not null,
+
+    foreign key (email) references tb_credenciais (email)
 );
